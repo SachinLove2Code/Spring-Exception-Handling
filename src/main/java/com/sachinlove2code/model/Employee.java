8c4e6d376cd8713d3f2891628a430683994ee443
SachinLove2Code <SachinLove2Code@users.noreply.github.com>
2018-08-18 23:37:39 +0530

    Creating spring boot project and model for employee

---
+++ src/main/java/com/sachinlove2code/model/Employee.java
@@ -0,0 +1,48 @@
+package com.sachinlove2code.model;
+
+public class Employee {
+
+	
+	private Long empId;
+	private String name;
+	private double salary;
+	private String designation;
+	
+	public Employee(){
+		
+	}
+
+	public Long getEmpId() {
+		return empId;
+	}
+
+	public void setEmpId(Long empId) {
+		this.empId = empId;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public double getSalary() {
+		return salary;
+	}
+
+	public void setSalary(double salary) {
+		this.salary = salary;
+	}
+
+	public String getDesignation() {
+		return designation;
+	}
+
+	public void setDesignation(String designation) {
+		this.designation = designation;
+	}
+	
+	
+}
